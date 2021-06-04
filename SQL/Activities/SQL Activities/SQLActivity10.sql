f7686cec4e54465a5e472e5bba88df6e6fc61706
SaiBhavaniJeebula <85238941+SaiBhavaniJeebula@users.noreply.github.com>
2021-06-04 18:55:55 +0530

    Add files via upload

---
+++ SQL/Activities/SQL Activities/SQLActivity10.sql
@@ -0,0 +1,24 @@
+REM   Script: Activity10
+REM   Update Table
+
+SELECT a.customer_name AS "Customer Name", a.city, b.name AS "Salesman", b.commission FROM customers a  
+INNER JOIN salesman b ON a.salesman_id=b.salesman_id  
+WHERE b.commission>12;
+
+SELECT a.order_no, a.order_date, a.purchase_amount, b.customer_name AS "Customer Name", b.grade, c.name AS "Salesman", c.commission FROM orders a  
+INNER JOIN customers b ON a.customer_id=b.customer_id  
+INNER JOIN salesman c ON a.salesman_id=c.salesman_id;
+
+SELECT * FROM orders 
+WHERE salesman_id=(SELECT DISTINCT salesman_id FROM orders WHERE customer_id=3007);
+
+SELECT * FROM orders 
+WHERE salesman_id IN (SELECT salesman_id FROM salesman WHERE city='New York');
+
+SELECT grade, COUNT(*) FROM customers 
+GROUP BY grade HAVING grade>(SELECT AVG(grade) FROM customers WHERE city='New York');
+
+SELECT order_no, purchase_amount, order_date, salesman_id FROM orders 
+WHERE salesman_id IN( SELECT salesman_id FROM salesman 
+WHERE commission=( SELECT MAX(commission) FROM salesman));
+
